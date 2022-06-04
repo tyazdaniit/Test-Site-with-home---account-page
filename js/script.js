c8e2506e5f563d333a0ee77732e28c3fac70e239
pinocchio1994 <tyazdani7@gmail.com>
2022-06-05 01:36:10 +0430

    s

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-$(document).ready(function() {
-    $("a[href^='#']").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function() {
-                window.location.hash = hash;
-            });
+$(document).ready(function () {
+  $("a[href^='#']").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000,
+        function () {
+          window.location.hash = hash;
         }
-    });
+      );
+    }
+  });
 });
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-
-    $('.btn-group-fab').on('click', function() {
-        $('.btn-group-fab').toggleClass('active');
-    });
+  $(".btn-group-fab").on("click", function () {
+    $(".btn-group-fab").toggleClass("active");
+  });
 });
